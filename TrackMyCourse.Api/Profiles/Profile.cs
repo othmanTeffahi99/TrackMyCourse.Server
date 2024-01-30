695d34edea57e289fb64833aacd1dce587760b26
othmane teffqhi <othmane.teffahi@outlook.com>
2024-01-30 15:06:38 +0100

    Add project files.

---
+++ TrackMyCourse.Api/Profiles/Profile.cs
@@ -0,0 +1,14 @@
+﻿using TrackMyCourseApi.Dtos;
+using TrackMyCourseApi.Dtos.CourseDtos;
+using trackmycourseapi.models;
+
+namespace TrackMyCourseApi.Profiles;
+
+public class Profile : AutoMapper.Profile
+{
+    public Profile()
+    {
+        CreateMap<Course, CourseDto>();
+        CreateMap<CourseCreateDto, Course>();
+    }
+}
---
+++ TrackMyCourse.Api/Repositories/RepositoryBase/EfRepository.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TrackMyCourseApi.Data;
+using TrackMyCourseApi.Repositories.Interfaces;
+
+namespace TrackMyCourseApi.Repositories.RepositoryBase;
+
+public class EfRepository<T>(AppDbContext dbContext) : RepositoriesBase<T>(dbContext) , IRepository<T> where T : class
+{
+    
+}
